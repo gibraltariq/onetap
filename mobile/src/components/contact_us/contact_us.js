848d8f2e2dace64f0c8974c99c9715af5e70ab01
Tariq Patanam <t.ahmed.p@gmail.com>
2019-05-25 06:31:07 -0700

    Basic input field, no styling

--- mobile/src/components/contact_us/contact_us.js
+++ mobile/src/components/contact_us/contact_us.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 type Props = {};
@@ -13,15 +13,24 @@ export default class ContactUs extends Component<Props> {
             Drop us your phone number and we'll be in touch. Don't worry, Onetap is free until you're ready to pay.
           </Text>
         </View>
-        <View>
-          <Text style={styles.ask}>An expert can text me at.</Text>
+        <View style={styles.fields}>
+          <Text style={styles.ask}>An expert can text me at: </Text>
+          <TextInput style={styles.input} placeholder="Pizzaaa"/>
         </View>
       </View>
     );
   }
 }
 
+const grayTextColor = '#828282';
+const bodyFontSize = 1.9;
+// 1.5 to deal with floating point precision.
+const bodyLineHeight = 1.5 * bodyFontSize;
 const styles = StyleSheet.create({
+  ask: {
+    color: grayTextColor,
+    fontSize: hp(bodyFontSize),
+  },
   container: {
     backgroundColor: '#FAFAFA',
     flex: 1,
@@ -29,16 +38,24 @@ const styles = StyleSheet.create({
     paddingVertical: hp('10%'),
     paddingHorizontal: wp('7%'),
   },
+  explanation: {
+    color: grayTextColor,
+    fontSize: hp(bodyFontSize),
+    fontStyle: 'italic',
+    lineHeight: hp(bodyLineHeight),
+    marginTop: hp(bodyFontSize),
+  },
+  fields: {
+    marginVertical: hp(5),
+  },
+  input: {
+    borderWidth: 2,
+    fontSize: bodyFontSize,
+    padding: hp(1),
+  },
   title: {
     color: '#4F4F4F',
     fontSize: hp('2.8%'),
     fontWeight: 'bold',
-    marginBottom: hp('1.9%')
   },
-  explanation: {
-    color: "#828282",
-    fontSize: hp('1.9%'),
-    fontStyle: 'italic',
-    lineHeight: hp('2.8%%'),
-  }
 });
