0ae68c0428fc4e2fd2da021d5751fad1a562e70c
Tariq Patanam <t.ahmed.p@gmail.com>
2019-05-24 21:48:11 -0700

    Splash page

---
+++ mobile/src/components/splash/splash.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class Splash extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image style={styles.logoImage} source={require('../../assets/logo.png')}/>
+          <Text style={styles.logoText}>Your travel agent on demand.</Text>
+        </View>
+      </View>
+    );
+  }
+}
+          //<Image source={require('../../assets/logo.png')} style={styles.logo}/>
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F96866',
+  },
+  logo: {
+    position: 'absolute',
+    top: '30%',
+    alignItems: 'center',
+  },
+  logoText: {
+    color: 'white',
+    paddingTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
