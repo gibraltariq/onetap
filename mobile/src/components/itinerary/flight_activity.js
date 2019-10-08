8a93c8418cd592893389274ff7ee6bcb84dd9f00
Tariq Patanam <t.ahmed.p@gmail.com>
2019-10-07 21:19:49 -0700

    [Mobile] Remove 'undefined' from flight activity details.
    Whenever cost was undefined we were getting undefined.

--- mobile/src/components/itinerary/flight_activity.js
+++ mobile/src/components/itinerary/flight_activity.js
@@ -15,7 +15,6 @@ export default class FlightActivity extends Component {
       <Activity
         {...this.props}
         iconImage={require('../../assets/plane.png')}
-        details={[`${this.props.cost}`]}
         backgroundColor={'#4FAFCE'}
       />
     );
