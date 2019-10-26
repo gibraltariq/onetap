e7670e2b440f41166088a052a78563fd03a8c346
Tariq Patanam <t.ahmed.p@gmail.com>
2019-10-25 17:36:47 -0700

    [Backend] Auto reply with Twilio message.

--- backend/controllers/tripRequestController.js
+++ backend/controllers/tripRequestController.js
@@ -1,11 +1,19 @@
 const airtable = require('../common').base;
 const Joi = require('@hapi/joi');
+const env = require('../env');
+const twilio = require('twilio')(env.twilio_sid, env.twilio_key);
 
 const DEFAULT_PLANNER = {
     "id": "usrzvformhSIDUbxg",
     "email": "onetaptravel@outlook.com",
     "name": "Onetap Travel",
 };
+
+const AUTO_REPLY = (name) => {
+  return `Hi ${name}! It's Onetap and we got your request. To help our ` +
+    `trip writers craft the perfect itinerary, fill out your ` +
+    `preferences here: https://forms.gle/FgthdDm9XbszDjW48`;
+}
 
 /** Validates a request from a user for a trip. */
 exports.tripRequestValidate = async (req, res, next) => {
@@ -37,14 +45,23 @@ exports.tripRequestPost = async (req, res, next) => {
         'planner': DEFAULT_PLANNER,
         'location_requested': location,
     });
-    res.sendStatus(200);
     next();
   } catch (error) {
-    res.status(400).send(`Error! Here are the details ${error}`)
+    next(error);
   }
 };
 
 /** Auto reply to a user when they post a request. */
 exports.tripRequestReply = async (req, res, next) => {
-
+  const {name, phoneNumber} = req.validatedBody;
+  try {
+    const result = await twilio.messages.create({
+      body: AUTO_REPLY(name),
+      from: env.twilio_number,
+      to: phoneNumber,
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(400).send(`Uh oh our Moroccan monkeys messed up our app! ${error}`)
+  }
 }
--- backend/env.js
+++ backend/env.js
@@ -3,7 +3,7 @@ const isDev = isNow === 'dev1';
 
 module.exports = {
     twilio_sid: isDev ? 'ACa45c98493bb668e8758c0dbe90b08878' : process.env.TWILIO_SID,
-    twilio_key: process.env.TWILIO_KEY, // defined in .env
+    twilio_key: isDev ? '9d74a7becbac8a5f540d416cc04b68ab': process.env.TWILIO_KEY, // defined in .env
     twilio_number: isDev ? '+15005550006' : process.env.TWILIO_NUMBER,
     airtable_key: process.env.AIRTABLE_KEY // defined in .env
 };
